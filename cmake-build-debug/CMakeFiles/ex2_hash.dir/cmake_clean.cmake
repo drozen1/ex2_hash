152e58dededa59255a2662e33fac516b6cbc3c63
drozen <drozen@campus.technion.ac.il>
2020-06-10 10:40:56 +0300

    add files

---
+++ cmake-build-debug/CMakeFiles/ex2_hash.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex2_hash.dir/main.cpp.obj"
+  "ex2_hash.exe"
+  "ex2_hash.exe.manifest"
+  "ex2_hash.pdb"
+  "libex2_hash.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex2_hash.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
